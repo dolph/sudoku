0e73bcb4342d245ed0c70b2daaaeeda2a3cf945d
Dolph Mathews <dolph.mathews@gmail.com>
2020-09-01 00:29:51 -0500

    rendering all choices to the board

--- sudoku.go
+++ sudoku.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"strconv"
 	"time"
 )
@@ -20,10 +18,6 @@ const six = 0b000100000
 const seven = 0b001000000
 const eight = 0b010000000
 const nine = 0b100000000
-
-type Board struct {
-	values [9][9]int
-}
 
 // These are used to convert back and forth between bitwise representations and integers.
 var bit2Int = map[int]int{
@@ -51,27 +45,101 @@ var int2Bit = map[int]int{
 	9: nine,
 }
 
+type Board struct {
+	values [9][9]int
+}
+
+func (b Board) PrintChoice(row int, col int, pos int) string {
+	if b.values[row][col]&int2Bit[pos] > 0 {
+		return strconv.Itoa(pos)
+	} else {
+		return " "
+	}
+}
+
+// ╔━━━━━━━╤━━━━━━━╤━━━━━━━╦━
+// ║ 1 2 3 | 1 2 3 | 1 2 3 ║
+// ║ 4 5 6 | 4 5 6 | 4 5 6 ║
+// ║ 7 8 9 | 7 8 9 | 7 8 9 ║
+// ╟───────┼───────┼───────╫─
 func (b Board) Print() {
 	for row := 0; row < 9; row++ {
-		if row%3 == 0 {
-			fmt.Println("+-------+-------+-------+")
+		if row == 0 {
+			fmt.Println("┏━━━━━━━┯━━━━━━━┯━━━━━━━┳━━━━━━━┯━━━━━━━┯━━━━━━━┳━━━━━━━┯━━━━━━━┯━━━━━━━┓")
+		} else if row%3 == 0 {
+			fmt.Println("┣━━━━━━━┿━━━━━━━┿━━━━━━━╋━━━━━━━┿━━━━━━━┿━━━━━━━╋━━━━━━━┿━━━━━━━┿━━━━━━━┫")
+		} else {
+			fmt.Println("┠───────┼───────┼───────╂───────┼───────┼───────╂───────┼───────┼───────┨")
 		}
-		for col := 0; col < 9; col++ {
-			if col%3 == 0 {
-				fmt.Printf("| ")
-			}
 
-			i := bit2Int[b.values[row][col]]
-			if i == 0 {
-				fmt.Printf("·")
-			} else {
-				fmt.Printf("%d")
+		// "┨"
+		// "┃"
+		for pos := 0; pos < 3; pos++ {
+			for col := 0; col < 9; col++ {
+				if col%3 == 0 {
+					fmt.Printf("┃")
+				} else {
+					fmt.Printf("│")
+				}
+				fmt.Printf(" %s %s %s ", b.PrintChoice(row, col, 3*pos+1), b.PrintChoice(row, col, 3*pos+2), b.PrintChoice(row, col, 3*pos+3))
 			}
-			fmt.Printf(" ")
+			fmt.Println("┃")
 		}
-		fmt.Println("|")
 	}
-	fmt.Println("+-------+-------+-------+")
+	fmt.Println("┗━━━━━━━┷━━━━━━━┷━━━━━━━┻━━━━━━━┷━━━━━━━┷━━━━━━━┻━━━━━━━┷━━━━━━━┷━━━━━━━┛")
+
+}
+
+func (b Board) PrintChoices(row int, col int, pass int) {
+	if pass == 0 {
+		if b.values[row][col]&one > 0 {
+			fmt.Printf("1")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&two > 0 {
+			fmt.Printf("2")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&three > 0 {
+			fmt.Printf("3")
+		} else {
+			fmt.Printf("·")
+		}
+	} else if pass == 1 {
+		if b.values[row][col]&four > 0 {
+			fmt.Printf("4")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&five > 0 {
+			fmt.Printf("5")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&six > 0 {
+			fmt.Printf("6")
+		} else {
+			fmt.Printf("·")
+		}
+	} else if pass == 2 {
+		if b.values[row][col]&seven > 0 {
+			fmt.Printf("7")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&eight > 0 {
+			fmt.Printf("8")
+		} else {
+			fmt.Printf("·")
+		}
+		if b.values[row][col]&nine > 0 {
+			fmt.Printf("9")
+		} else {
+			fmt.Printf("·")
+		}
+	}
 }
 
 func initialize() Board {
@@ -89,24 +157,6 @@ func initialize() Board {
 
 func main() {
 	board := initialize()
-
-	board.Print()
-
-	// Populate board
-	fmt.Println("Populate the board left to right, top to bottom:")
-	reader := bufio.NewReader(os.Stdin)
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			fmt.Print("(", i, ",", j, "-> ")
-			text, _ := reader.ReadString('\n')
-			intValue, _ := strconv.Atoi(text)
-			if intValue, ok := int2Bit[intValue]; ok {
-				board.values[i][j] = int2Bit[intValue]
-			} else {
-				fmt.Println("1-9 only")
-			}
-		}
-	}
 
 	board.Print()
 }
